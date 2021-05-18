acd73d81add61242c4171538ccd273ad0ee5b719
Bilal0147 <79477194+Bilal0147@users.noreply.github.com>
2021-05-18 11:49:00 +0500

    Add files via upload

---
+++ client.c
@@ -0,0 +1,44 @@
+/**
+ * Example client program that uses thread pool.
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "threadpool.h"
+
+struct data {
+    int a;
+    int b;
+};
+
+void add(void *param) {
+    struct data *temp;
+    temp = (struct data *)param;
+    printf("I add two values %d and %d result = %d\n", temp->a, temp->b, temp->a + temp->b);
+}
+
+int main(void) {
+    // create some work to do
+    int n = 0,i;
+    while(n <= 0) {
+        printf("Please input the number of tasks: ");
+        scanf("%d", &n);
+    }
+    // initialize the thread pool
+    pool_init();
+    // submit the work to the queue
+    struct data *params = malloc(n * sizeof(struct data));
+    for(i = 0; i != n; ++i) {
+        params[i].a = i;
+        params[i].b = -i;
+        while(pool_submit(&add, &params[i]) != 0) {
+        	
+        }
+    }
+    printf("All tasks have been submitted, wait for a while to exit.\n");
+    sleep(3);
+    pool_shutdown();
+    free(params);
+    return 0;
+}
