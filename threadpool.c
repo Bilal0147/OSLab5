1d883350fbf835e9ae40ae634b49fa541d83c68f
Bilal0147 <79477194+Bilal0147@users.noreply.github.com>
2021-06-01 14:40:36 +0500

    Update threadpool.c

--- threadpool.c
+++ threadpool.c
@@ -4,8 +4,8 @@
 #include <semaphore.h>
 #include "threadpool.h"
 
-#define QUEUE_SIZE 10
-#define NUMBER_OF_THREADS 4
+#define QUEUE_SIZE 20
+#define NUMBER_OF_THREADS 5
 
 #define TRUE 1
 
@@ -21,9 +21,9 @@ typedef struct
 }task;
 
 task queue[QUEUE_SIZE];
-int queuenum = 0;
-int deqcurpos = 0;
-int enqcurpos = 0;
+int a = 0;
+int b = 0;
+int c = 0;
 
 // the work queue
 task worktodo;
@@ -35,6 +35,7 @@ pthread_t bee1;
 pthread_t bee2;
 pthread_t bee3;
 pthread_t bee4;
+pthread_t bee5;
 
 int choosethread = 0;
 
@@ -43,13 +44,13 @@ int choosethread = 0;
 int enqueue(task t)
 {
     sem_wait(&semth);
-    if(queuenum > 10){
+    if(a > 20){
 	sem_post(&semth);
 	return 1;
     }
-    queue[enqcurpos] = t;
-    queuenum ++;
-    enqcurpos = (enqcurpos + 1)%10;
+    queue[c] = t;
+    a ++;
+    c = (c + 1)%20;
     sem_post(&semth);
     return 0;
 }
@@ -57,8 +58,8 @@ int enqueue(task t)
 // remove a task from the queue
 task dequeue()
 {
-    worktodo = queue[deqcurpos];
-    deqcurpos = (deqcurpos + 1)%10;
+    worktodo = queue[b];
+    b = (b + 1)%20;
     return worktodo;
 }
 
@@ -72,11 +73,11 @@ void *worker(void *param)
 {
     task nwork;
     sem_wait(&semth);
-    if(queuenum != 0){
-	queuenum--;
+    if(a != 0){
+	a--;
 	nwork = dequeue();
 	execute((*nwork.function),(nwork.data));
-	//queuenum--;
+	
     }
     sem_post(&semth);
     pthread_exit(0);
@@ -100,8 +101,11 @@ int pool_submit(void (*somefunction)(void *p), void *p)
 	pthread_create(&bee3,NULL,worker,NULL);
     }else if(choosethread == 3){
 	pthread_create(&bee4,NULL,worker,NULL);
+    }else if(choosethread == 4){
+	pthread_create(&bee5,NULL,worker,NULL);
     }
-    choosethread = (choosethread + 1)%4;
+
+    choosethread = (choosethread + 1)%5;
 
     return eq;
 }
@@ -112,6 +116,7 @@ void pool_init(void)
     pthread_create(&bee2,NULL,worker,NULL);
     pthread_create(&bee3,NULL,worker,NULL);
     pthread_create(&bee4,NULL,worker,NULL);
+    pthread_create(&bee5,NULL,worker,NULL);
     sem_init(&semth,0,1);
 }
 // shutdown the thread pool
@@ -121,4 +126,5 @@ void pool_shutdown(void)
     pthread_join(bee2,NULL);
     pthread_join(bee3,NULL);
     pthread_join(bee4,NULL);
+    pthread_join(bee5,NULL);
 }
